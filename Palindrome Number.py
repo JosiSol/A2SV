8098bd3672299adb518a33c0c43c26d7c99fff37
Yosef Solomon <121563505+JosiSol@users.noreply.github.com>
2024-03-20 21:50:57 +0300

    Create Palindrome Number.py
    Hackerrank

---
+++ Palindrome Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x > 0 and x % 10 == 0):
+            return False
+        r = 0
+        while(x > r):
+            r = r * 10 + x % 10
+            x = x // 10
+        return True if (x == r or x == r // 10) else False
